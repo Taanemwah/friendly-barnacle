b270a48cfa9e35dc8509ff14013564fe8614e17f
Taanemwah <23mcintosht@mountainview.school.nz>
2025-07-16 12:57:09 +1200

    products and numbers

--- start.py
+++ start.py
@@ -1,11 +1,9 @@
-import random
-intMysteryNumber=random.randint(0,100)
-for intCounter in range(100):
-    intGuess=int(input("Guess the number: "))
-    if intGuess == intMysteryNumber:
-        print("You are a genius")
-        break;
-    if intMysteryNumber > intGuess:
-        print("The number is higher")
-    else:
-        print("The number is lower")
+GST_RATE = 0.15
+productName = input("Enter product name ")
+productPrice = input("Enter unit price of product ")
+amountPurchased = int(input("Enter amount purchased "))
+saleTotal = int(productPrice) * int(amountPurchased)
+print("product name:" + productName)
+print("price: " + str(productPrice) )
+print("amount purchased: " + str(amountPurchased))
+print("Total sales: " + str(saleTotal))
