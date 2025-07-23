a4270d4d872fa7cdf7029d3a0a29348d555330c9
Taanemwah <23mcintosht@mountainview.school.nz>
2025-07-23 12:24:30 +1200

    workPT1

---
+++ school2nd.py
@@ -0,0 +1,6 @@
+SchoolBag=int(input("Enter The School Bag Price: "))
+StationeryPack=int(input("Enter The Stationery Pack Price: "))
+SportsUniform=int(input("Enter The Sports Uniform Price: "))
+TotalCost= SchoolBag+StationeryPack+SportsUniform
+print("School Bag Price: " + str(SchoolBag) + " Stationery Pack Price: " + str(StationeryPack) + " Sports Uniform Price: " + str(SportsUniform))
+print("Total Cost: " + str(TotalCost))
