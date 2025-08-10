e86630ee60419a8d2b8f67e3808f44872bad1e59
Taanemwah <23mcintosht@mountainview.school.nz>
2025-08-11 10:32:46 +1200

    change

--- runningtit.py
+++ runningtit.py
@@ -1,6 +1,6 @@
 RunningTotal = 0.0
 for x in range(4):
-    ProductDesc = input("Enter product description: ")
+    ProductDesc = float(input("Enter Amount sold: "))
     ProductPrice = float(input("Enter product price: "))
     RunningTotal += ProductPrice
-    print("Total: ", RunningTotal)
+print("Total: ", RunningTotal)
