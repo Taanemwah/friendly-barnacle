d1cbcb71a39118a22968fe5461bdfcda89939056
Taanemwah <23mcintosht@mountainview.school.nz>
2025-08-19 11:28:36 +1200

    school space

--- school.py
+++ school.py
@@ -3,5 +3,6 @@ SchoolUniform=int(input("Enter The School Uniform Price: "))
 SportsGear=int(input("Enter The Sports Gear Price: "))
 Textbooks=int(input("Enter The Textbooks Price: "))
 TotalCost= Textbooks+SportsGear+SchoolUniform+TuitionFees
+
 print("Tuition Fees Price: " + str(TuitionFees) + " School Uniform Price: " + str(SchoolUniform) + " Sports Gear Price: " + str(SportsGear) + " Textbooks Price: " + str(Textbooks))
 print("Total Cost: " + str(TotalCost))
